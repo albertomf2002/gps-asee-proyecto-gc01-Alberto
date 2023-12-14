c12c9338b7756c3301e0ad6894a8c113cc391199
Oscar <obaoscam@alumnos.unex.es>
2023-12-14 21:00:03 +0100

    Corrigiendo error.

--- app/src/main/java/es/unex/giiis/asee/totalemergency/view/home/RecordDetailViewModel.kt
+++ app/src/main/java/es/unex/giiis/asee/totalemergency/view/home/RecordDetailViewModel.kt
@@ -1,4 +1,35 @@
 package es.unex.giiis.asee.totalemergency.view.home
 
-class RecordDetailViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import es.unex.giiis.asee.totalemergency.TotalEmergencyApplication
+import es.unex.giiis.asee.totalemergency.data.Repository
+import es.unex.giiis.asee.totalmergency.data.model.VideoRecord
+
+class RecordDetailViewModel(
+    private val repository: Repository,
+) : ViewModel() {
+
+    var path : String? = null
+    var video : VideoRecord? = null
+
+    init {
+
+    }
+    companion object {
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras
+            ): T {
+                val application =
+                    checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
+                return RecordDetailViewModel(
+                    (application as TotalEmergencyApplication).appContainer.repository
+                ) as T
+            }
+        }
+    }
 }
